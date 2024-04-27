f5618801b88914484fa73bd2da6d288f1f1f2f49
Mikhail-Leonidovich <mikhail.yurkevich.blr@gmail.com>
2024-04-27 17:38:53 +0300

    some changes

--- js/main.js
+++ js/main.js
@@ -3,16 +3,30 @@ const allCircles = document.querySelectorAll('.about__circle');
 const root = document.querySelector(':root');
 const rootStyles = getComputedStyle(root);
 
+const handleChangeNavLinks = (currentItem) => {
+  allNavLinks.forEach((item) => {
+    item === currentItem ? currentItem.classList.add('nav__link--active') : item.classList.remove('nav__link--active');
+  })
+}
+
 allNavLinks.forEach((item) => {
   item.addEventListener('click', () => {
     handleChangeNavLinks(item);
   })
 })
 
-const handleChangeNavLinks = (currentItem) => {
-  allNavLinks.forEach((item) => {
-    item === currentItem ? currentItem.classList.add('nav__link--active') : item.classList.remove('nav__link--active');
-  })
+const handleChangeCircles = (item, currentPercent) => {
+  let span = item.firstElementChild;
+  if (item.id === span.innerHTML) {
+    span.innerHTML = currentPercent;
+  } else {
+    span.innerHTML = item.id;
+  }
+}
+
+const handleGetCirclesPercent = (item) => {
+  let currentPercent = rootStyles.getPropertyValue(`--${item.id}-firstColor`);
+  return currentPercent;
 }
 
 allCircles.forEach((item) => {
@@ -26,16 +40,4 @@ allCircles.forEach((item) => {
   })
 })
 
-const handleGetCirclesPercent = (item) => {
-  let currentPercent = rootStyles.getPropertyValue(`--${item.id}-firstColor`);
-  return currentPercent;
-}
 
-const handleChangeCircles = (item, currentPercent) => {
-  let span = item.firstElementChild;
-  if (item.id === span.innerHTML) {
-    span.innerHTML = currentPercent;
-  } else {
-    span.innerHTML = item.id;
-  }
-}
