d4f80d191e83558d23cbb30c8980397d1ebf066a
Mikhail-Leonidovich <mikhail.yurkevich.blr@gmail.com>
2024-04-30 20:17:57 +0300

    add smooth-scrolling and animation circleText

--- js/main.js
+++ js/main.js
@@ -1,27 +1,35 @@
 const allNavLinks = document.querySelectorAll('.nav__link');
 const allCircles = document.querySelectorAll('.about__circle');
+const allSections = document.querySelectorAll('section');
+
 const root = document.querySelector(':root');
 const rootStyles = getComputedStyle(root);
 
+const handleMoveToSection = (currentItem) => {
+  let href = currentItem.getAttribute('href').replace('#', '');
+  allSections.forEach((item) => {
+    if (href === item.id) {
+      item.scrollIntoView({ behavior: "smooth" ,block: 'start'});
+    }
+  })
+}
+
 const handleChangeNavLinks = (currentItem) => {
   allNavLinks.forEach((item) => {
-    item === currentItem ? currentItem.classList.add('nav__link--active') : item.classList.remove('nav__link--active');
+    (item === currentItem) ? currentItem.classList.add('nav__link--active') : item.classList.remove('nav__link--active');
   })
 }
 
 allNavLinks.forEach((item) => {
   item.addEventListener('click', () => {
     handleChangeNavLinks(item);
+    handleMoveToSection(item);
   })
 })
 
 const handleChangeCircles = (item, currentPercent) => {
   let span = item.firstElementChild;
-  if (item.id === span.innerHTML) {
-    span.innerHTML = currentPercent;
-  } else {
-    span.innerHTML = item.id;
-  }
+  (item.id === span.innerHTML) ? span.innerHTML = currentPercent : span.innerHTML = item.id;
 }
 
 const handleGetCirclesPercent = (item) => {
@@ -31,7 +39,6 @@ const handleGetCirclesPercent = (item) => {
 
 allCircles.forEach((item) => {
   let currentPercent = handleGetCirclesPercent(item);
-
   item.addEventListener('mouseenter', () => {
     handleChangeCircles(item, currentPercent);
   })
@@ -39,5 +46,6 @@ allCircles.forEach((item) => {
     handleChangeCircles(item, currentPercent);
   })
 })
+
 
 
